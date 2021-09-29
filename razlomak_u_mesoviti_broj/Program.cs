7b04a5064d8cd47f077b462b27be7f855184d7d8
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-29 10:29:26 +0200

    Add files via upload

---
+++ razlomak_u_mesoviti_broj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace razlomak_u_mesoviti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = a / b;
+            d = a%b;
+            
+            Console.Write("{0} {1} {2}",d,c,b);
+
+        }
+    }
+}
